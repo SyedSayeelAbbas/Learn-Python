cf086313d30c7d6d6829842ba6ec07bdaf8dc86a
Syed Sayeel Abbas <syedsayeelabbas@gmail.com>
2025-09-23 12:41:21 +0500

    Create If and Elif and else.py

---
+++ String And Condition/If and Elif and else.py
@@ -0,0 +1,17 @@
+# Writing a single line Program to print "Hello World" 
+print("Hello World")
+#This will prit lines on different lines as we can see below
+print("Hello World")
+print("Iam learning Python")
+#This will print lines on the same line by using a comma between the statements
+print("Hello World" , "Iam Learing Python")
+#print numbers
+print(1,2,3,4,5)
+#sum of two number in print function
+print(2+2)
+
+a=2
+a=str("2")
+b="8"
+#type converstion
+print("The Sum of 2 and 2 is " , a,b)
